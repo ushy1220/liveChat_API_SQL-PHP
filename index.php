5f2ea0f6614e8bbfa14f0ae58f0f4905c6599ede
Michal Bielecki <michalpatrykbielecki@gmail.com>
2023-02-11 15:35:34 +0100

    coment ifs

--- index.php
+++ index.php
@@ -1,66 +1,16 @@
 <?php
+    session_start();
+
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: *");
     header("Access-Control-Allow-Methods: *");
 
-    /*
-    session_start();
-
-    if(isset($_POST['email'])) {
-        $correct = true;
-        $login = $_POST['login'];
-
-        //walidacja długości loginu
-        if((@strlen($login)<3) || (@strlen($login)>20)) {
-            $correct = false;
-            $_SESSION['e_login']="Login needs to consist of minimum 3 and maximum 20 signs";
-        }
-
-        //walidacja poprawności rodzaju znaków
-        if(@ctype_alnum($login)==false) {
-            $correct = false;
-            $_SESSION['e_login']="Username needs to consist of only alphanumeric signs";
-        }
-
-        //walidacja email
-        $email = $_POST['email'];
-        $emailB = filter_var($email, FILTER_SANITIZE_EMAIL);
-        if((filter_var($emailB, FILTER_SANITIZE_EMAIL)==false) || ($emailB!=$email)) {
-            $correct = false;
-            $_SESSION['e_email']="Nieprawidłowy adres email!";
-        }
-
-        //walidacja hasła
-        $password1 = $_POST['password1'];
-        $password2 = $_POST['password2'];
-
-        if((strlen($password1)<8) || (strlen($password1)>20)) {
-            $correct = false;
-            $_SESSION['e_password']="Hasło musi zawierać od 8 do 20 znaków!";
-        }
-
-        if($password1 != $password2) {
-            $correct = false;
-            $_SESSION['e_password']="Podane hasła nie są identyczne!";
-        }
-
-        //zabezpieczenie przed odczytem hasła
-        $password_hash = password_hash($password1, PASSWORD_DEFAULT);
-
-        //walidacja checkboxa
-        if(!isset($_POST['checkbox'])) {
-            $correct = false;
-            $_SESSION['e_checkbox']="Zaakceptuj regulamin";
-        }
-
         //walidacja reCAPTCHA
         //dekodowanie $check
-
         //walidacja powtórzenia loginu
-        require_once "DbConnect.php";
-        */
+
 
         //      PDO
 
@@ -72,7 +22,62 @@
         switch($method) {
 
             case "POST":
+
+                /*  Warunki                
+                
+
+                if(isset($_POST['email'])){
+                    $correct=true;
+
+                    //LOGIN
+                    $login = $_POST['login'];
+
+                    if((@strlen($login)<3) || (@strlen($login)>20)) {
+                        $correct=false;
+                    }
+
+                    if(@ctype_alnum($login)==false) {
+                        $correct=false;
+                    }
+
+                    //EMAIL
+                    $email = $_POST['email'];
+                    $emailB = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+                    if((filter_var($emailB, FILTER_VALIDATE_EMAIL)==false) || ($emailB!=$email)) {
+                        $correct=false;
+                    }
+
+                    //PASSWORD
+                    $password1 = $_POST['password'];
+                    $password2 = $_POST['password2'];
+
+                    if((strlen($password1)<8) || (strlen($password1)>20)) {
+                        $correct=false;
+                    }
+
+                    if($password1 != $password2){
+                        $correct=false;
+                    }
+
+                    $password_hash = password_hash($password1, PASSWORD_DEFAULT);
+
+                    //CHECKBOX
+                    if(!isset($_POST['checkbox'])) {
+                        $correct=false;
+                    }
+
+                    //Walidacja ostateczna
+                    if($correct = true){
+                /*
+                */
+
+            //if(isset($_POST['email'])){
+                //$correct=true;
+
                 $user = json_decode( file_get_contents('php://input') );
+
+                
                 $sql = "INSERT INTO users(id, login, email, password, username, created_at) VALUES(null, :login, :email, :password, :username, :created_at)";
                 $stmt = $conn->prepare($sql);
                 $created_at = date('Y-m-d');
@@ -89,12 +94,21 @@
                 }
                 echo json_encode($response);
                 break;
-            }
+            } 
+        //}
         
+        /*
+        } 
+        */
+   
+        /*
+         }
+        */
 
         ?>
 
         <?php
+
         /*
         mysqli_report(MYSQLI_REPORT_STRICT);
 
